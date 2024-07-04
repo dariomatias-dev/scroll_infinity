7478a5b6ca39065d7966f59ae18b23e1e0898d57
dariomatias-dev <matiasdario75@gmail.com>
2024-07-04 07:48:58 -0300

    feat(lib/src): separated into its own file `ScrollInfinityInitialItemsNotifier`
    - Element `ScrollInfinityInitialItemsNotifier` moved to its own file, aiming at the separation of elements.

--- lib/src/scroll_infinity_loader.dart
+++ lib/src/scroll_infinity_loader.dart
@@ -1,25 +1,5 @@
 part of 'scroll_infinity.dart';
 
-class ScrollInfinityInitialItemsNotifier<T> extends ValueNotifier<List<T>?> {
-  ScrollInfinityInitialItemsNotifier(
-    super.value,
-  );
-
-  bool _hasError = false;
-
-  bool get hasError => _hasError;
-
-  void update({
-    required List<T>? items,
-    bool hasError = false,
-  }) {
-    value = items;
-    _hasError = hasError;
-
-    notifyListeners();
-  }
-}
-
 class ScrollInfinityLoader<T> extends StatelessWidget {
   const ScrollInfinityLoader({
     super.key,
