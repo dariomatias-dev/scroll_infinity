343a8fb7d813700223d97d6b84013c074636ca7b
dariomatias-dev <matiasdario75@gmail.com>
2024-12-25 10:53:09 -0300

    feat(example): re-added previously removed platforms
    - Previously removed platforms have been re-added to the package example.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:scroll_infinity/scroll_infinity.dart';
+
+final _random = Random();
 
 class ScrollInfinityExample extends StatefulWidget {
   const ScrollInfinityExample({super.key});
@@ -60,13 +65,125 @@ class _ScrollInfinityExampleState extends State<ScrollInfinityExample> {
               Align(
                 alignment: Alignment.center,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const InfiniteScrollExample();
+                        },
+                      ),
+                    );
+                  },
                   child: const Text('Access'),
                 ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class InfiniteScrollExample extends StatefulWidget {
+  const InfiniteScrollExample({super.key});
+
+  @override
+  State<InfiniteScrollExample> createState() => _InfiniteScrollExampleState();
+}
+
+class _InfiniteScrollExampleState extends State<InfiniteScrollExample> {
+  final _notifier = InitialItemsNotifier<Color>(null);
+
+  static const maxItems = 10;
+
+  Future<void> _initLoader() async {
+    final items = await _loadData(0);
+
+    _notifier.update(
+      items: items,
+      hasError: items == null,
+    );
+  }
+
+  Future<List<Color>?> _loadData(
+    int pageIndex,
+  ) async {
+    await Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    );
+
+    if (_random.nextInt(3) == 0) {
+      return null;
+    }
+
+    return List.generate(
+      maxItems,
+      (index) {
+        return Color.fromARGB(
+          255,
+          _random.nextInt(255),
+          _random.nextInt(255),
+          _random.nextInt(255),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    _initLoader();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new_rounded,
+          ),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ScrollInfinityLoader(
+              notifier: _notifier,
+              scrollInfinityBuilder: (items) {
+                return ScrollInfinity(
+                  maxItems: 10,
+                  disableInitialRequest: true,
+                  initialPageIndex: 0,
+                  initialItems: items,
+                  interval: 2,
+                  loadData: _loadData,
+                  itemBuilder: (value, index) {
+                    if (value == null) {
+                      return const SizedBox(
+                        height: 100.0,
+                        child: Placeholder(),
+                      );
+                    }
+
+                    return Container(
+                      height: 100.0,
+                      color: value,
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
